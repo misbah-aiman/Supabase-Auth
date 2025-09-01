6df8cc2101375ca8cec61405fbb16d9174b650e0
Misbah Aiman <misbahaiman652gmail.com>
2025-09-01 21:28:03 +0500

    updated dashboard

--- src/router.jsx
+++ src/router.jsx
@@ -3,18 +3,10 @@ import App from "./App";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import Dashboard from "./components/Dashboard";
-import PrivateRoute from "./components/PrivateRoute";
 
 export const router = createBrowserRouter([
   { path: "/", element: <App /> },
   { path: "/signUp", element: <SignUp /> },
   { path: "/signIn", element: <SignIn /> },
-  {
-    path: "/dashboard",
-    element: (
-      <PrivateRoute>
-        <Dashboard />
-      </PrivateRoute>
-    ),
-  },
+  { path: "/dashboard", element: <Dashboard /> },
 ]);
